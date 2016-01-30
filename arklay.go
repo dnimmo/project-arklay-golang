6206b434a07e96c2200e20645f19d3b8f5d58fe6
David Nimmo <dnimmo@gmail.com>
2016-01-30 01:53:04 +0000

    intro added

---
+++ arklay.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("Your head hurts. You're not sure where you are, and you definitely don't know how you got here.\nYou can hear rain thrashing the ground outside, but the only exit is locked anyway.\nYou figure you might as well try and understand what the Hell is going on...\n")
+}
